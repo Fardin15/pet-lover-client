01d343fdffa83f7ac52141bdebe7ba7ac49795f8
Fardin15 <mohammedshowrav15@gmail.com>
2024-06-02 08:43:43 +0600

    navbar added

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,9 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layouts/Main/Main";
+import Home from "../Pages/Home/home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    errorElement: <p>error</p>,
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 
