ddcf7a53de6d14039f6571083e030947015ef309
Fardin15 <mohammedshowrav15@gmail.com>
2024-06-10 00:27:26 +0600

    contact section added on home page

--- src/Pages/Home/home/Home.jsx
+++ src/Pages/Home/home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../../../Components/HomeBanner/Banner";
 import AboutUs from "../AboutUs/AboutUs";
+import Contact from "../Contact/Contact";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <AboutUs></AboutUs>
+      <Contact></Contact>
     </div>
   );
 };
